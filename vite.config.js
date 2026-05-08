5dddcd064963436c563b2d33f96c7bd425297d6c
jijan <azizanartiandiakbar@gmail.com>
2026-05-09 00:32:50 +0700

    ignored venv

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
   ],
   server: {
     host: true,
+    watch: {
+      ignored: ['**/venv/**']
+    },
     allowedHosts: [
       "tradeplanner.my.id",
       ".tradeplanner.my.id"
