da1c63ffa88c9e2faef30238c0dba7c275585a6a
jijan <azizanartiandiakbar@gmail.com>
2026-06-10 21:41:37 +0700

    fix production routing

--- src/App.jsx
+++ src/App.jsx
@@ -1357,6 +1357,7 @@ function App() {
               <Route path="/cookie-policy" element={<CookiePolicy />} />
               <Route path="/api-docs" element={<APIDocs />} />
               <Route path="/status" element={<StatusPage />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </div>
           {/* 📱 MOBILE BOTTOM NAVIGATION */}
